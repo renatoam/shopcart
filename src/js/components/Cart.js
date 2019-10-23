d410240779a6a925b45053a8c8328086a9f183bb
Renato Melo <renatoalves92@gmail.com>
2019-10-22 22:15:50 -0300

    Fixing stock. Pt I

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,116 +1,161 @@
+// Importing functions that fix images not loading
 import FixImage from "../handlers/FixImage";
 
+// Importing API for requests
+import api from './../services';
+
 class Cart {
-  constructor() {
-      this.cart = [];
-  }
+    constructor() {
+        this.cart = [];
+    }
 
-  addToCart(obj) {
-      let iconCart = document.querySelector('#qtd');
-      let objects = [];
-      
-      this.cart.push(obj);
-      let unique = new Set(this.cart);
-      
-      unique.forEach(el => {
-          let qtd = this.cart.filter(f => f.id === el.id).length;
-          objects.push({ el, qtd });
-      });
+    // Show item count on Header (cart icon) 
+    countItems(items) {
+        document.querySelector('#qtd').textContent = items;
+    }
 
-      iconCart.textContent = this.cart.length;
-      this.mountCart(objects);
-  }
+    updateStock(id, qtd) {
+        api.patch(`/${id}`, {
+            quantity: qtd
+        });
+    }
 
-  removeFromCart(obj) {
-      let iconCart = document.querySelector('#qtd');
-      let objects = [];
+    async addToCart(obj) {
+        let stockQtd = obj.quantity - 1
 
-      let del = this.cart.findIndex(el => el.id === obj.id);
-      
-      this.cart.splice(del, 1);
-      let unique = new Set(this.cart);
-      
-      unique.forEach(el => {
-          let qtd = this.cart.filter(f => f.id === el.id).length;
-          objects.push({ el, qtd });
-      });
-      
-      iconCart.textContent = this.cart.length;
-      this.mountCart(objects);
-  }
+        if (obj.quantity > 0) {
+            this.cart.push(obj);
+            this.updateStock(obj.id, stockQtd);
+            this.countItems(this.cart.length);
+            this.mountCart(obj);
+        } else {
+            return;
+        }
+    }
 
-  clearRender() {
-      let sideCart = document.querySelector('#cart');
-      sideCart.innerHTML = "";
-  }
+    removeFromCart(obj) {
+        let iconCart = document.querySelector('#qtd');
+        let objects = [];
 
-  calculate() {
-    let vlTotal = 0;
-    this.cart.forEach(el => vlTotal += el.price - (el.price * 0.2));
+        let del = this.cart.findIndex(el => el.id === obj.id);
 
-    return vlTotal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-  }
+        this.cart.splice(del, 1);
+        let unique = new Set(this.cart);
 
-  mountCart(objects) {
-      let render = '';
-      
-      if (objects.length > 0) {
-        objects.forEach(p => {
+        unique.forEach(el => {
+            let qtd = this.cart.filter(f => f.id === el.id).length;
+            objects.push({ el, qtd });
+        });
+
+        iconCart.textContent = this.cart.length;
+        this.mountCart(objects);
+    }
+
+    clearRender() {
+        let sideCart = document.querySelector('#cart');
+        sideCart.innerHTML = "";
+    }
+
+    calculate() {
+        let vlTotal = 0;
+        this.cart.forEach(el => vlTotal += el.price - (el.price * 0.2));
+
+        return vlTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    }
+
+    mountCart(obj) {
+        let render = '';
+
+        if (obj.quantity > 0) {
             this.clearRender();
-            let currentPrice = (p.el.price - (p.el.price * 0.2)).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
-            let logo = '';
-            let brand = p.el.brand;
-            switch (brand) {
-                case 'Motorola': logo = './../../src/assets/moto.png';
-                break;
-                case 'LG': logo = './../../src/assets/lg.png';
-                break;
-                case 'Samsung': logo = './../../src/assets/samsung.png';
-                break;
-                case 'Lenovo': logo = './../../src/assets/lenovo.png';
-                break;
-                default: logo = './../../src/assets/chibi-naruto.png'
-            }
-            render += `
+                let currentPrice = (obj.price - (obj.price * 0.2)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+                let logo = '';
+                let brand = obj.brand;
+                switch (brand) {
+                    case 'Motorola': logo = './../../src/assets/moto.png';
+                        break;
+                    case 'LG': logo = './../../src/assets/lg.png';
+                        break;
+                    case 'Samsung': logo = './../../src/assets/samsung.png';
+                        break;
+                    case 'Lenovo': logo = './../../src/assets/lenovo.png';
+                        break;
+                    default: logo = './../../src/assets/chibi-naruto.png'
+                }
+                render += `
             <section class="bag">
-                <section class="bag__item" data-id="${p.el.id}">
+                <section class="bag__item" data-id="${obj.id}">
                     <figure>
-                        <img src="${p.el.picture}" />
+                        <img src="${obj.picture}" />
                     </figure>
                     <ul class="bag__info">
-                        <li>${p.el.title}</li>
+                        <li>${obj.title}</li>
                         <li>${currentPrice}</li>
                         <li><img src="${logo}" alt="Logo ${brand}" style="${brand == 'Samsung' && "width: 45px"}"/></li>
                     </ul>
                     <section class="bag__actions">
-                        <button class="add" data-id="${p.el.id}">+</button>
-                        <span>${p.qtd}</span>
-                        <button class="remove" data-id="${p.el.id}">-</button>
+                        <button class="add" data-id="${obj.id}">+</button>
+                        <span>${obj.quantity++}</span>
+                        <button class="remove" data-id="${obj.id}">-</button>
                     </section>
                 </section>
             </section>`;
-        });
-      } else {
-          this.clearRender()
-      }
+            // obj.forEach(p => {
+            //     this.clearRender();
+            //     let currentPrice = (p.el.price - (p.el.price * 0.2)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+            //     let logo = '';
+            //     let brand = p.el.brand;
+            //     switch (brand) {
+            //         case 'Motorola': logo = './../../src/assets/moto.png';
+            //             break;
+            //         case 'LG': logo = './../../src/assets/lg.png';
+            //             break;
+            //         case 'Samsung': logo = './../../src/assets/samsung.png';
+            //             break;
+            //         case 'Lenovo': logo = './../../src/assets/lenovo.png';
+            //             break;
+            //         default: logo = './../../src/assets/chibi-naruto.png'
+            //     }
+            //     render += `
+            // <section class="bag">
+            //     <section class="bag__item" data-id="${p.el.id}">
+            //         <figure>
+            //             <img src="${p.el.picture}" />
+            //         </figure>
+            //         <ul class="bag__info">
+            //             <li>${p.el.title}</li>
+            //             <li>${currentPrice}</li>
+            //             <li><img src="${logo}" alt="Logo ${brand}" style="${brand == 'Samsung' && "width: 45px"}"/></li>
+            //         </ul>
+            //         <section class="bag__actions">
+            //             <button class="add" data-id="${p.el.id}">+</button>
+            //             <span>${p.qtd}</span>
+            //             <button class="remove" data-id="${p.el.id}">-</button>
+            //         </section>
+            //     </section>
+            // </section>`;
+            // });
+        } else {
+            this.clearRender()
+        }
 
-      this.renderCart(render);
-  }
+        this.renderCart(render);
+    }
 
-  renderCart(render) {
-    let sideCart = document.querySelector('#cart');
-    let contentCart = '';
-    let total = `
+    renderCart(render) {
+        let sideCart = document.querySelector('#cart');
+        let contentCart = '';
+        let total = `
     <section class="bag__total">
         <p>Total(${this.cart.length == 1 ? this.cart.length + " item" : this.cart.length + " itens"}): </p>
         <p>${this.calculate()}</p>
     </section>
     `;
 
-    contentCart = render + total;
-    sideCart.insertAdjacentHTML('afterbegin', contentCart);
-    FixImage();
-  }
+        contentCart = render + total;
+        sideCart.insertAdjacentHTML('afterbegin', contentCart);
+        FixImage();
+    }
 }
 
 export default Cart;
--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -1,27 +1,30 @@
-// imports
-// let Product = require('./Product');
-// let Cart = require('./Cart');
+// importing product and cart control modules
 import Product from './Product';
 import Cart from './Cart';
 
+// Application Control Class
 class ShopCart {
     constructor() {
         this.cart = new Cart;
         this.product = new Product;
     }
 
+    // Render products and attach events to dynamic elements
     mount() {
         this.product.request()
         this.addEvent()
     }
 
+    // Attach events to dynamic elements
     addEvent() {
         document.addEventListener('click', e => {
             let target = e.target;
             let obj = this.product.elements.filter(el => el.id == target.getAttribute('data-id'));
+            // Verify all elements that match target element ID and creates an array "obj"
             
             target.classList.contains('add') && this.cart.addToCart(obj[0]);
             target.classList.contains('remove') && this.cart.removeFromCart(obj[0]);
+            // Verify by class name if it add or remove an item and pass as argument only one item from array obj
         });
     }
 }
